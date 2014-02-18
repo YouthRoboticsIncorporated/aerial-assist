d40f4f9b5e3f770209ca4deec475f9ee70129c09
Lucien Morey <lucienp@live.com.au>
2014-02-18 18:47:20 +1100

    changed the variable names recieved form the bbb

--- src/commands/RunTracking.cpp
+++ src/commands/RunTracking.cpp
@@ -11,9 +11,9 @@ void RunTracking::Initialize(){
 void RunTracking::Execute(){
 	beaglebone->readData();
 	SmartDashboard::PutBoolean("Beaglebone Connection", beaglebone->isConnected);
-	SmartDashboard::PutNumber("Goal x", beaglebone->goalX);
-	SmartDashboard::PutNumber("Goal Y", beaglebone->goalY);
+	SmartDashboard::PutNumber("Goal Range", beaglebone->goalRange);
 	SmartDashboard::PutNumber("Goal angle", beaglebone->goalAngle);
+	SmartDashboard::PutNumber("Goal Hot", beaglebone->goalHot);
 	SmartDashboard::PutNumber("Ball x", beaglebone->ballX);
 	SmartDashboard::PutNumber("Ball Y", beaglebone->ballY);
 	SmartDashboard::PutNumber("Ball diam", beaglebone->ballDiam);
